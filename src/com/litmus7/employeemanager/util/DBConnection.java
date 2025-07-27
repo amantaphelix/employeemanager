924ea2fbefbe27c60f856c494f7906a4ed5077f0
amantaphelix <amantaphelix@gmail.com>
2025-07-27 13:13:29 +0530

    updated assignment

--- src/com/litmus7/employeemanager/util/DBConnection.java
+++ src/com/litmus7/employeemanager/util/DBConnection.java
@@ -1,27 +1,17 @@
 package com.litmus7.employeemanager.util;
 
-import java.io.FileInputStream;
+import com.litmus7.employeemanager.property.DatabaseProperties;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
 
 public class DBConnection {
 
-    private static final String PROPERTIES_FILE = "db.properties";
-
     public static Connection getConnection() throws SQLException {
-        Properties props = new Properties();
-
-        try (FileInputStream input = new FileInputStream(PROPERTIES_FILE)) {
-            props.load(input);
-        } catch (Exception e) {
-            throw new RuntimeException("Failed to load database properties", e);
-        }
-
-        String url = props.getProperty("db.url");
-        String username = props.getProperty("db.username");
-        String password = props.getProperty("db.password");
+        String url = DatabaseProperties.getDbUrl();
+        String username = DatabaseProperties.getDbUser();
+        String password = DatabaseProperties.getDbPassword();
 
         return DriverManager.getConnection(url, username, password);
     }
